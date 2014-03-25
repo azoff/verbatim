a6958f6b2bbb8f2e5cfe35a4bb78d95a681c9a64
Jonathan Azoff <jon@azoffdesign.com>
2014-03-24 23:33:13 -0700

    first commit

---
+++ verbatim/screens/CPCaptionScreen.h
@@ -0,0 +1,13 @@
+//
+//  CPCaptionScreen.h
+//  groupproject
+//
+//  Created by Jonathan Azoff on 3/24/14.
+//  Copyright (c) 2014 Jonathan Azoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPCaptionScreen : UIViewController
+
+@end
