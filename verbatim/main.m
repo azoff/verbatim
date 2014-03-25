ebcb1b843b3508de06d9e9ebc5e05ee5d4e1b6cc
Jonathan Azoff <jon@azoffdesign.com>
2014-03-24 23:26:39 -0700

    Initial Commit

---
+++ verbatim/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  verbatim
+//
+//  Created by Jonathan Azoff on 3/24/14.
+//  Copyright (c) 2014 Verbatim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VBAppDelegate class]));
+    }
+}
