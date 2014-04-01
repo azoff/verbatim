6d92de1a93c199a42a1087ee43b4112f970d0382
nick <nick@halper.de>
2014-04-01 11:47:31 -0700

    First integration of voice recognition for dragon SDK
    added table view for history of messaging
    
    Initial refactoring and creation of VBInputSourceManager
    
    Cleaned up code preparing for push

--- verbatim/controllers/VBCaptionController.m
+++ verbatim/controllers/VBCaptionController.m
@@ -7,10 +7,16 @@
 //
 
 #import "VBCaptionController.h"
+#import "VBInputSourceManager.h"
 
 @interface VBCaptionController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *captionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *recordingStateLabel;
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong,nonatomic) NSMutableArray *captions;
+
 
 @end
 
@@ -25,10 +31,56 @@
     return self;
 }
 
+- (NSArray *)captions
+{
+    if (!_captions) {
+        _captions = [@[] mutableCopy];
+    }
+    return _captions;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [VBSpeechKit setupWithDelegate:self];
+    
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    
+    // subscribe to channel for debug listening
+    [[NSNotificationCenter defaultCenter]
+     addObserverForName:VBInputSourceManagerDidFinishWithResultsNotification
+     object:nil
+     queue:nil
+     usingBlock:^(NSNotification *notification) {
+         NSLog(@"Caption controller received notification: %@",notification.userInfo[@"best"]);
+         self.caption = notification.userInfo[@"best"];
+     }];
+    
+}
+
+-(void)setCaption:(NSString *)caption
+{
+    _caption = caption;
+    self.captionLabel.text = caption;
+    [self.captions addObject:caption];
+    [self.tableView reloadData];
+    [self.tableView setContentOffset:CGPointMake(0, self.tableView.contentSize.height - self.tableView.frame.size.height)];
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.captions count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+    }
+    cell.textLabel.text = [self.captions objectAtIndex:indexPath.row];
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning
