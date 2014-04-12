54767e50af1673fd47ac4d7eb0b32eeaf38658b7
cahlering <cahlering@richrelevance.com>
2014-04-12 07:51:23 -0700

    Add of VBColor class, just with a placeholder for VBGreen at this point

---
+++ verbatim/core/VBColor.h
@@ -0,0 +1,15 @@
+//
+//  VBColor.h
+//  verbatim
+//
+//  Created by Chris Ahlering on 4/12/14.
+//  Copyright (c) 2014 Verbatim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBColor : UIColor
+
++ (UIColor *)vbGreenColor;
+
+@end
---
+++ verbatim/core/VBColor.m
@@ -0,0 +1,17 @@
+//
+//  VBColor.m
+//  verbatim
+//
+//  Created by Chris Ahlering on 4/12/14.
+//  Copyright (c) 2014 Verbatim. All rights reserved.
+//
+
+#import "VBColor.h"
+
+@implementation VBColor
+
++ (UIColor *)vbGreenColor {
+    return [UIColor colorWithRed:0 green:1 blue:0 alpha:0];
+}
+
+@end
