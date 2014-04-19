2bfd341184ebcfe9ee6bf14c4bf8e06b14a4742b
Jonathan Azoff <jon@azoffdesign.com>
2014-04-19 11:28:25 -0700

    VBNavigation Controller Polish
    - Deletes VBBarButtonItem and creates VBButton (for IB)
    - Reworks the navigation bar to support a static header
    - Adds support for accessing the rootController from VBViewControllers
    - Adds global handlers for nav button clicks
    - Adds basic fade animation for transition

---
+++ verbatim/views/VBButton.h
@@ -0,0 +1,15 @@
+//
+//  VBBarButton.h
+//  verbatim
+//
+//  Created by Jonathan Azoff on 4/17/14.
+//  Copyright (c) 2014 Verbatim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBButton : UIButton
+
+@property (nonatomic) UIColor *overlayColor;
+
+@end
